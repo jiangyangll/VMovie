395add122a56a0c40425785e70d28e28acf92e28
DravenForProgram <DravenForProgram@gmail.com>
2016-09-02 14:32:57 +0800

    系列详情视频list

---
+++ VMovie/app/src/main/java/com/example/dllo/vmovie/series/SeriesDescVideoListFragment.java
@@ -0,0 +1,26 @@
+package com.example.dllo.vmovie.series;
+
+import android.view.View;
+
+import com.example.dllo.vmovie.R;
+import com.example.dllo.vmovie.base.BaseFragment;
+
+/**
+ * Created by dllo on 16/9/2.
+ */
+public class SeriesDescVideoListFragment extends BaseFragment {
+    @Override
+    protected int setLayout() {
+        return R.layout.fragment_series_desc_video_list;
+    }
+
+    @Override
+    protected void initView(View view) {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
